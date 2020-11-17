68b3bebf3bebc58df01d0985667ef2995349dee5
root <root@localhost.localdomain>
2020-11-17 13:47:01 +0800

    midify pullImage.sh

--- pullImage.sh
+++ pullImage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ELK_VERSION=$1
 docker pull docker.elastic.co/elasticsearch/elasticsearch:$ELK_VERSION
 docker pull docker.elastic.co/logstash/logstash:$ELK_VERSION
 docker pull docker.elastic.co/kibana/kibana:$ELK_VERSION
